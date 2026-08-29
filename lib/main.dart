37500a66781a4f827f24d4159496e756057858b7
LEE YUN JAE <155600506+KhankhanLee@users.noreply.github.com>
2026-08-29 14:16:28 +0900

    main.dart 업데이트

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_insta_clone/insta_home.dart';
+import 'package:Assence/insta_home.dart';
 
 void main() => runApp(new MyApp());
 
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Instagram',
+      title: 'Assence',
       debugShowCheckedModeBanner: false,
       theme: new ThemeData(
           primarySwatch: Colors.blue,
