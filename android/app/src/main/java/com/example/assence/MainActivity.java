1e31c8313731e1b65197c62aadd91928952bb167
LEE YUN JAE <155600506+KhankhanLee@users.noreply.github.com>
2026-08-30 11:29:06 +0900

    MainActivity.java 업데이트

--- android/app/src/main/java/com/example/assence/MainActivity.java
+++ android/app/src/main/java/com/example/assence/MainActivity.java
@@ -1,13 +1,6 @@
 package com.example.assence;
 
-import android.os.Bundle;
 import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
 }
