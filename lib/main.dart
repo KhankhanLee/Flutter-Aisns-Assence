d4c2fa7b2c7721f99ce0ad0c7c7f7d9bddbacd53
LEE YUN JAE <155600506+KhankhanLee@users.noreply.github.com>
2026-08-30 10:07:32 +0900

    main.dart 업데이트

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:assence/assence_home.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
+    return MaterialApp(
       title: 'Assence',
       debugShowCheckedModeBanner: false,
-      theme: new ThemeData(
-          primarySwatch: Colors.blue,
-          primaryColor: Colors.black,
-          buttonColor: Colors.black,
-          primaryIconTheme: IconThemeData(color: Colors.black),
-          primaryTextTheme: TextTheme(
-              bodyText1: TextStyle(color: Colors.black, fontFamily: "Aveny")),
-          textTheme: TextTheme(bodyText1: TextStyle(color: Colors.black))),
-      home: new AssenceHome(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Colors.black,
+        // buttonColor 대신 colorScheme을 사용합니다.
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue).copyWith(
+          primary: Colors.black,
+        ),
+        primaryIconTheme: const IconThemeData(color: Colors.black),
+        // bodyText1 -> bodyLarge로 변경합니다.
+        primaryTextTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Colors.black, fontFamily: "Aveny"),
+        ),
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Colors.black),
+        ),
+      ),
+      home: const AssenceHome(),
     );
   }
 }
